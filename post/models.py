a020594edd0a0ed1f536bf486b7d22743bcf086f
Alex Yin (I.T.S <AlexYin@bdo.com.hk>
2016-11-17 17:24:16 +0800

    added reply

--- post/models.py
+++ post/models.py
@@ -3,9 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model):  
+class IRepliable(models.Model):
+  pass
+
+class Post(IRepliable):  
   writer = models.ForeignKey(User, related_name='posts', blank = False)  
   title = models.TextField(blank = False)
+  content = models.TextField(blank = True)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+class Reply(IRepliable): 
+  to = models.ForeignKey(IRepliable, related_name='replies')
+  writer = models.ForeignKey(User, related_name='replies', blank = False)  
   content = models.TextField(blank = True)
   created_at = models.DateTimeField(auto_now_add=True)
 
