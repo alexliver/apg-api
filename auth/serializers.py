42c0a73d221eec4f8cd15e37ff8d6f241ba34c2a
Sisi He <Sisi He>
2016-12-29 17:23:57 +0400

    register

--- auth/serializers.py
+++ auth/serializers.py
@@ -6,3 +6,12 @@ class SignUpSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username', 'password')
         write_only_fields = ('password',)
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+        
